940a5fe1f3dc9bb63d2698af2b700a2e134a2aac
MomoPewpew <marijntepas@gmail.com>
2025-02-14 18:12:28 +0100

    Prevent ability activation after item drop
    - Add drop timeout mechanism to AbilityListener
    - Disable left-click abilities briefly after dropping an item
    - Prevent accidental ability triggers during item management

--- src/main/kotlin/net/sneakyprototypekit/ability/AbilityListener.kt
+++ src/main/kotlin/net/sneakyprototypekit/ability/AbilityListener.kt
@@ -9,15 +9,39 @@ import org.bukkit.event.player.PlayerItemConsumeEvent
 import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.persistence.PersistentDataType
 import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.event.EventPriority
 
 /**
  * Listens for item and food interactions to trigger abilities.
  */
 class AbilityListener : Listener {
+    private val dropTimeouts = mutableMapOf<String, Long>()
+    private val DROP_TIMEOUT_MS = 150L // How long to disable abilities after dropping
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    fun onPlayerDropItem(event: PlayerDropItemEvent) {
+        val item = event.itemDrop.itemStack
+        val meta = item.itemMeta ?: return
+        val container = meta.persistentDataContainer
+        val plugin = SneakyPrototypeKit.getInstance()
+
+        // If this is an item with a left click ability, set a timeout
+        if (container.has(plugin.LEFT_CLICK_ABILITY_KEY, PersistentDataType.STRING)) {
+            dropTimeouts[event.player.uniqueId.toString()] = System.currentTimeMillis() + DROP_TIMEOUT_MS
+        }
+    }
 
     @EventHandler
     fun onPlayerInteract(event: PlayerInteractEvent) {
         if (event.action != Action.LEFT_CLICK_AIR && event.action != Action.LEFT_CLICK_BLOCK) return
+        if (!event.hasItem()) return
+        
+        // Check if we're in the drop timeout period
+        val timeout = dropTimeouts[event.player.uniqueId.toString()]
+        if (timeout != null && System.currentTimeMillis() < timeout) {
+            return
+        }
         
         val item = event.item ?: return
         val meta = item.itemMeta ?: return
