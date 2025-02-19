3c3876c2c2219595b57ac9d5395d2f0132fdbe0d
MomoPewpew <marijntepas@gmail.com>
2025-02-19 06:44:42 +0100

    Downgrade Paper API and server version to 1.20.6
    - Update build.gradle.kts to use Paper API 1.20.6
    - Adjust server version in runServer configuration
    - Revert previous version bump to 1.21.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
 }
 
 tasks {
@@ -39,7 +39,7 @@ tasks {
     }
     
     runServer {
-        minecraftVersion("1.21.4")
+        minecraftVersion("1.20.6")
     }
 
     compileJava {
